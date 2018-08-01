d9571ec78478d851e8dd47b83bf24b6e1c8157cf
gutian <gutian@guazi.com>
2018-08-01 16:42:31 +0800

    添加备案号

--- resources/views/widget/footer.blade.php
+++ resources/views/widget/footer.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="row text-center">
             <ul>
+                <li><a href="http://www.beian.gov.cn/portal/index.do" target="_blank">冀ICP备17020318号-2</a> </li>
             </ul>
 
         </div>
